746ce7b1eab8dcd3e72d89737c548321877c3d93
argorar <sebas199500@hotmail.com>
2018-12-06 23:27:58 -0500

    Move folders and add Day 3-1

---
+++ Day-3/D3-1.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const input = fs.readFileSync('assets/claimsAreas', 'utf-8')
+
+var formatedInput = input.trim()
+                           .split('\n')
+                           .map(string => string)
+
+var fabric = new Map()
+var counter=0
+formatedInput.forEach(element => {
+    let aux = element.split(" ")
+    let auxLocation = aux[2].replace(":","").split(",")
+    let auxSize= aux[3].split("x")
+    var box ={ left: parseInt(auxLocation[0]),
+               top:  parseInt(auxLocation[1]),
+               wide: parseInt(auxSize[0]),
+               tall: parseInt(auxSize[1])}
+    let auxTop =box.top
+    let auxLeft=box.left
+    for (let i = 0; i < box.wide; i++) {
+        for (let j = 0; j < box.tall; j++) {
+            let coordenada = auxTop+""+auxLeft
+            if(fabric.has(coordenada))
+                fabric.set(coordenada,2)
+            else
+                fabric.set(coordenada,1)
+            auxLeft++            
+        }
+        auxLeft=box.left   
+        auxTop++
+    }    
+})
+fabric.forEach(coordenada => {
+    if(coordenada>1)
+        counter++
+});
+console.log(counter)//correct 109785
